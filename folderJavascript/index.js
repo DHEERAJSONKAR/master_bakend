a81547b748f040f26b776d57a1e6295c391645b0
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-12-04 12:51:25 +0530

    Add initial HTML and JavaScript files with basic functionality

---
+++ folderJavascript/index.js
@@ -0,0 +1,34 @@
+// fundamental of js
+// arrays and objects
+// function return
+// async js coding
+
+
+const arr = [1,2,3,4,4,5,6];
+arr.forEach(function(val){
+    console.log(val + " Dheeraj Sonkar")
+})
+
+const newarray = arr.map(function(val){
+    return val * 2;
+})
+console.log(newarray) 
+
+const Array = arr.filter(function(val){
+    if(val >=3) return true;
+    else return false;
+})
+console.log(Array)
+
+const findarray = arr.find(function(val){
+    if (val ===4) return val;
+})
+console.log(findarray)
+
+
+const obj = {
+    name: "Dheeraj Sonkar",
+    age: 24
+}
+obj.age = 22;
+console.log(obj)
