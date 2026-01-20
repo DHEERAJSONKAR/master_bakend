73ca34200e09995f998896573a02f717a3c8a603
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-01-20 10:33:25 +0530

    Set up Express server with MongoDB connection; add user, owner, and product models; create routers for user, owner, and product routes; update .gitignore to include .env

---
+++ scatch/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+const Path = require("path");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(Path.join(__dirname,"public")));
+app.set("view engine","ejs");
+const mongooseConnection = require("./config/mongoose-connection");
+
+app.use("/owners", ownerRouter)
+app.use("/users", userRouter)
+app.use("/products", productRouter)
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
---
+++ scatch/config/mongoose-connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/scratchDB")
+.then(function(){
+    console.log("Connected to MongoDB successfully");
+})
+.catch(function(err){
+    console.log("Error connecting to MongoDB:", err);
+}); 
+module.exports = mongoose.connection;
