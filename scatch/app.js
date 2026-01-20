8fb13701a048ef707bf6ec6ba0f001ce64217b19
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2026-01-20 22:08:02 +0530

    Refactor routing structure: replace user, owner, and product routers with new ownersRouter, usersRouter, and productsRouter; remove old router files

--- scatch/app.js
+++ scatch/app.js
@@ -9,9 +9,9 @@ app.use(express.static(Path.join(__dirname,"public")));
 app.set("view engine","ejs");
 const mongooseConnection = require("./config/mongoose-connection");
 
-app.use("/owners", ownerRouter)
-app.use("/users", userRouter)
-app.use("/products", productRouter)
+app.use("/owners", ownersRouter)
+app.use("/users", usersRouter)
+app.use("/products", productsRouter)
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
---
+++ scatch/routes/ownersRouter.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+router.get("/",(req,res)=>{
+    res.send("Owner Home Page");
+})
+module.exports = router; 
